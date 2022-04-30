06411240dfea7d936ae2a8f75ba5ca3bef164ecb
YukiWatanabe824 <gyggg950@gmail.com>
2022-04-30 11:13:53 +0900

    DOMの組み立てをerbに移行/エスケープ処理のタイミングを画面表示時に変更

--- memo.rb
+++ memo.rb
@@ -16,13 +16,12 @@ get '/' do
 end
 
 get '/memos/' do
-  @hash = ''
+  @hash = []
   memofiles = Dir.glob('memos/*').sort
   memofiles.each do |memofile|
     File.open(memofile, 'r') do |memodata|
       JSON.parse(memodata.read).each_value do |memojson|
-        @hash += "<li>#{h(memojson['title'])} : #{h(memojson['content'])}"
-        @hash += " <a href=\"http://localhost:4567/memos/memo#{h(memojson['id'])}/edit\">修正する</a> </li>"
+        @hash << memojson
       end
     end
   end
@@ -35,31 +34,23 @@ end
 
 post '/memos/' do
   maxid = 0
-  oldmemos = []
   memofiles = Dir.glob('memos/*')
-  oldmemos = memofiles.map{|memofile| memofile.gsub('memos/memo', '').to_i}
- # memofiles.each do |memofile|
- #   oldmemos << memofile.gsub('memos/memo', '').to_i
- # end
+  oldmemos = memofiles.map { |memofile| memofile.gsub('memos/memo', '').to_i }
   oldmemos.each do |oldmemo|
     maxid = oldmemo if oldmemo > maxid
   end
 
   File.open("memos/memo#{maxid + 1}.json", 'w') do |file|
-    file.print({ "memo#{maxid + 1}" => { 'id' => maxid + 1, 'title' => h(params[:title]), 'content' => h(params[:content]) } }.to_json)
+    file.print({ "memo#{maxid + 1}" => { id: maxid + 1, title: params[:title], content: params[:content] } }.to_json)
   end
   redirect '/memos/new/'
 end
 
 get '/memos/:memo/edit' do |m|
-  @title = ''
-  @content = ''
-  @id = ''
+  @memo = {}
   File.open("memos/#{m}.json", 'r') do |memodata|
     JSON.parse(memodata.read).each_value do |memojson|
-      @id = memojson['id']
-      @title = memojson['title']
-      @content = memojson['content']
+      @memo = memojson
     end
   end
   erb :edit_memo
@@ -67,12 +58,12 @@ end
 
 patch '/memos/:memo/edit' do
   File.open("memos/memo#{params[:id]}.json", 'w') do |file|
-    file.print({ "memo#{params[:id]}" => { 'id' => params[:id], 'title' => h(params[:title]), 'content' => h(params[:content]) } }.to_json)
+    file.print({ "memo#{params[:id]}" => { id: params[:id], title: params[:title], content: params[:content] } }.to_json)
   end
   redirect "/memos/memo#{params[:id]}/edit"
 end
 
-delete '/memos/delete/:memo' do
+delete '/memos/:memo/delete' do
   File.delete("memos/memo#{params[:id]}.json")
   redirect '/memos/'
 end
